f8b6efa5cb0007d8ffd6d065b47813aa423decf4
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2023-09-28 07:46:15 -0300

    refactor: modularização validaCadastro

--- src/assets/js/validaCadastro.js
+++ src/assets/js/validaCadastro.js
@@ -1,118 +1,30 @@
+import { validaInput } from "./validaInput.js";
+import { addEventListener } from "./eventListener.js";
+
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("form");
-    const nomeInput = document.getElementById("nome");
-    const emailInput = document.getElementById("email");
-    const cpfInput = document.getElementById("cpf");
-    const idadeInput = document.getElementById("idade");
-    const sexoInput = document.getElementById("sexo");
-    const ruaInput = document.getElementById("rua");
-    const bairroInput = document.getElementById("bairro");
-    const cidadeInput = document.getElementById("cidade");
-    const estadoSelect = document.getElementById("estado");
 
-    const alertaNome = document.getElementById("alertaNome");
-    const alertaEmail = document.getElementById("alertaEmail");
-    const alertaCpf = document.getElementById("alertaCpf");
-    const alertaIdade = document.getElementById("alertaIdade");
-    const alertaSexo = document.getElementById("alertaSexo");
-    const alertaRua = document.getElementById("alertaRua");
-    const alertaBairro = document.getElementById("alertaBairro");
-    const alertaCidade = document.getElementById("alertaCidade");
-    const alertaEstado = document.getElementById("alertaEstado");
+    const campos = [
+        { input: document.getElementById("nome"), alerta: document.getElementById("alertaNome") },
+        { input: document.getElementById("email"), alerta: document.getElementById("alertaEmail") },
+        { input: document.getElementById("cpf"), alerta: document.getElementById("alertaCpf") },
+        { input: document.getElementById("idade"), alerta: document.getElementById("alertaIdade") },
+        { input: document.getElementById("sexo"), alerta: document.getElementById("alertaSexo") },
+        { input: document.getElementById("rua"), alerta: document.getElementById("alertaRua") },
+        { input: document.getElementById("bairro"), alerta: document.getElementById("alertaBairro") },
+        { input: document.getElementById("cidade"), alerta: document.getElementById("alertaCidade") },
+        { input: document.getElementById("estado"), alerta: document.getElementById("alertaEstado") }
+    ];
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
 
-        alertaNome.style.display = "none";
-        alertaEmail.style.display = "none";
-        alertaCpf.style.display = "none";
-        alertaIdade.style.display = "none";
-        alertaSexo.style.display = "none";
-        alertaRua.style.display = "none";
-        alertaBairro.style.display = "none";
-        alertaCidade.style.display = "none";
-        alertaEstado.style.display = "none";
-
-        if (nomeInput.value === "") {
-            alertaNome.style.display = "block";
-        }
-        if (emailInput.value === "") {
-            alertaEmail.style.display = "block";
-        }
-        if (cpfInput.value === "") {
-            alertaCpf.style.display = "block";
-        }
-        if (idadeInput.value === "") {
-            alertaIdade.style.display = "block";
-        }
-        if (sexoInput.value === "") {
-            alertaSexo.style.display = "block";
-        }
-        if (ruaInput.value === "") {
-            alertaRua.style.display = "block";
-        }
-        if (bairroInput.value === "") {
-            alertaBairro.style.display = "block";
-        }
-        if (cidadeInput.value === "") {
-            alertaCidade.style.display = "block";
-        }
-        if (estadoSelect.value === "") {
-            alertaEstado.style.display = "block";
-        }
+        campos.forEach(({ input, alerta }) => {
+            validaInput(input, alerta);
+        });
     });
 
-    nomeInput.addEventListener("input", function () {
-        if (nomeInput.value !== "") {
-            alertaNome.style.display = "none";
-        }
-    });
-
-    emailInput.addEventListener("input", function () {
-        if (emailInput.value !== "") {
-            alertaEmail.style.display = "none";
-        }
-    });
-
-    cpfInput.addEventListener("input", function () {
-        if (cpfInput.value !== "") {
-            alertaCpf.style.display = "none";
-        }
-    });
-
-    idadeInput.addEventListener("input", function () {
-        if (idadeInput.value !== "") {
-            alertaIdade.style.display = "none";
-        }
-    });
-
-    sexoInput.addEventListener("input", function () {
-        if (sexoInput.value !== "") {
-            alertaSexo.style.display = "none";
-        }
-    });
-
-    ruaInput.addEventListener("input", function () {
-        if (ruaInput.value !== "") {
-            alertaRua.style.display = "none";
-        }
-    });
-
-    bairroInput.addEventListener("input", function () {
-        if (bairroInput.value !== "") {
-            alertaBairro.style.display = "none";
-        }
-    });
-
-    cidadeInput.addEventListener("input", function () {
-        if (cidadeInput.value !== "") {
-            alertaCidade.style.display = "none";
-        }
-    });
-
-    estadoSelect.addEventListener("change", function () {
-        if (estadoSelect.value !== "") {
-            alertaEstado.style.display = "none";
-        }
+    campos.forEach(({ input, alerta }) => {
+        addEventListener(input, alerta);
     });
 });
