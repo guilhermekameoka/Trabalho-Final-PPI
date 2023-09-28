96daf228e5dc792171fc306d26828f3f2143f98f
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2023-09-28 07:46:02 -0300

    refactor: modularização validaAgendamento

--- src/assets/js/validaAgendamento.js
+++ src/assets/js/validaAgendamento.js
@@ -1,84 +1,27 @@
+import { validaInput } from "./validaInput.js";
+import { addEventListener } from "./eventListener.js";
+
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("form");
-    const especialidadeInput = document.getElementById("especialidade");
-    const dataInput = document.getElementById("data");
-    const horarioInput = document.getElementById("horario");
-    const nomeInput = document.getElementById("nome");
-    const emailInput = document.getElementById("email");
-    const sexoInput = document.getElementById("sexo");
 
-    const alertaEspecialidade = document.getElementById("alertaEspecialidade");
-    const alertaData = document.getElementById("alertaData");
-    const alertaHorario = document.getElementById("alertaHorario");
-    const alertaNome = document.getElementById("alertaNome");
-    const alertaEmail = document.getElementById("alertaEmail");
-    const alertaSexo = document.getElementById("alertaSexo");
-
+    const campos = [
+        { input: document.getElementById("nome"), alerta: document.getElementById("alertaNome") },
+        { input: document.getElementById("email"), alerta: document.getElementById("alertaEmail") },
+        { input: document.getElementById("especialidade"), alerta: document.getElementById("alertaEspecialidade") },
+        { input: document.getElementById("data"), alerta: document.getElementById("alertaData") },
+        { input: document.getElementById("horario"), alerta: document.getElementById("alertaHorario") },
+        { input: document.getElementById("sexo"), alerta: document.getElementById("alertaSexo") }
+    ];
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
 
-        alertaEspecialidade.style.display = "none";
-        alertaData.style.display = "none";
-        alertaHorario.style.display = "none";
-        alertaNome.style.display = "none";
-        alertaEmail.style.display = "none";
-        alertaSexo.style.display = "none";
-
-        if (nomeInput.value === "") {
-            alertaNome.style.display = "block";
-        }
-        if (emailInput.value === "") {
-            alertaEmail.style.display = "block";
-        }
-        if (sexoInput.value === "") {
-            alertaSexo.style.display = "block";
-        }
-        if (especialidadeInput.value === "") {
-            alertaEspecialidade.style.display = "block";
-        }
-        if (dataInput.value === "") {
-            alertaData.style.display = "block";
-        }
-        if (horarioInput.value === "") {
-            alertaHorario.style.display = "block";
-        }
+        campos.forEach(({ input, alerta }) => {
+            validaInput(input, alerta);
+        });
     });
 
-    especialidadeInput.addEventListener("input", function () {
-        if (especialidadeInput.value !== "") {
-            alertaEspecialidade.style.display = "none";
-        }
+    campos.forEach(({ input, alerta }) => {
+        addEventListener(input, alerta);
     });
-
-    dataInput.addEventListener("input", function () {
-        if (dataInput.value !== "") {
-            alertaData.style.display = "none";
-        }
-    });
-
-    horarioInput.addEventListener("input", function () {
-        if (horarioInput.value !== "") {
-            alertaHorario.style.display = "none";
-        }
-    });
-
-    nomeInput.addEventListener("input", function () {
-        if (nomeInput.value !== "") {
-            alertaNome.style.display = "none";
-        }
-    });
-
-    emailInput.addEventListener("input", function () {
-        if (emailInput.value !== "") {
-            alertaEmail.style.display = "none";
-        }
-    });
-
-    sexoInput.addEventListener("input", function () {
-        if (sexoInput.value !== "") {
-            alertaSexo.style.display = "none";
-        }
-    });
-
 });
