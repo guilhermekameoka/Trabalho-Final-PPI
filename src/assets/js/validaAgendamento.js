24d79c9fc682ba457f37d889c5a80c1b92e4cddb
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2023-10-02 18:06:27 -0300

    refactor: adicionado novos ids dos novos inputs

--- src/assets/js/validaAgendamento.js
+++ src/assets/js/validaAgendamento.js
@@ -8,6 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
         { input: document.getElementById("nome"), alerta: document.getElementById("alertaNome") },
         { input: document.getElementById("email"), alerta: document.getElementById("alertaEmail") },
         { input: document.getElementById("especialidade"), alerta: document.getElementById("alertaEspecialidade") },
+        { input: document.getElementById("data-nascimento"), alerta: document.getElementById("alertaDataNascimento") },
+        { input: document.getElementById("nomeMedico"), alerta: document.getElementById("alertaNomeMedico") },
+        { input: document.getElementById("telefone"), alerta: document.getElementById("alertaTelefone") },
         { input: document.getElementById("data"), alerta: document.getElementById("alertaData") },
         { input: document.getElementById("horario"), alerta: document.getElementById("alertaHorario") },
         { input: document.getElementById("sexo"), alerta: document.getElementById("alertaSexo") }
