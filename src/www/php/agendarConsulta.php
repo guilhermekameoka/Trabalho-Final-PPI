86ff37eae356aa2077427e4275e14b0d9010dd55
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2023-11-11 23:50:37 -0300

    style: organização do código

--- src/www/php/agendarConsulta.php
+++ src/www/php/agendarConsulta.php
@@ -22,17 +22,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pdo->beginTransaction();
 
     try {
-        // Insere dados na tabela agenda_funcionario
-        $agenda_funcionario = <<<SQL
-        INSERT INTO agenda_funcionario (nome, sexo, data_consulta, hora_consulta)
-        VALUES (?, ?, ?, ?)
-    SQL;
-
-        $stmt_agenda_funcionario = $pdo->prepare($agenda_funcionario);
-        if (!$stmt_agenda_funcionario->execute([$nome, $sexo, $data_consulta, $hora_consulta])) {
-            throw new Exception('Falha na inserção na tabela agenda_funcionário');
-        }
-
         // Insere dados na tabela consulta
         $consulta = <<<SQL
         INSERT INTO consulta (especialidade, profissional, data_consulta, horario, convenio, nome_paciente, data_nascimento, sexo, email, telefone)
@@ -44,6 +33,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             throw new Exception('Falha na inserção na tabela consulta');
         }
 
+        // Insere dados na tabela agenda_funcionario
+        $agenda_funcionario = <<<SQL
+        INSERT INTO agenda_funcionario (nome, sexo, data_consulta, hora_consulta)
+        VALUES (?, ?, ?, ?)
+        SQL;
+
+        $stmt_agenda_funcionario = $pdo->prepare($agenda_funcionario);
+        if (!$stmt_agenda_funcionario->execute([$nome, $sexo, $data_consulta, $hora_consulta])) {
+            throw new Exception('Falha na inserção na tabela agenda_funcionário');
+        }
 
         // Insere dados na tabela agenda_paciente
         $agenda_paciente = <<<SQL
@@ -62,7 +61,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Redireciona para a homepage
         // header("location: ../templates/private/funcionario/homeFuncionario.php");
         exit();
-        
+
     } catch (Exception $e) {
         // Rollback em caso de falha
         $pdo->rollback();
