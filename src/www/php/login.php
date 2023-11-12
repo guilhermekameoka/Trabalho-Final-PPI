8c9217eedd9a07e2b93e37dce793a0b15357cc85
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2023-11-12 03:19:37 -0300

    feat: adicionado action e method para login

---
+++ src/www/php/login.php
@@ -0,0 +1,56 @@
+<?php
+require "conexao.php";
+$pdo = mysqlConnect();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $email = $_POST["email"] ?? "";
+    $senha = $_POST["senha"] ?? "";
+
+    try {
+        $pdo->beginTransaction();
+
+        // Verifica se é um paciente
+        $consulta_paciente = "SELECT * FROM paciente WHERE email = ?";
+        $stmt_paciente = $pdo->prepare($consulta_paciente);
+        $stmt_paciente->execute([$email]);
+
+        if ($stmt_paciente->rowCount() > 0) {
+            $dados_paciente = $stmt_paciente->fetch(PDO::FETCH_ASSOC);
+            
+            // Verifica a correspondência da senha hash
+            if (password_verify($senha, $dados_paciente['senha'])) {
+                // Paciente autenticado com sucesso
+                // Redirecionar para a homepage do paciente
+                header("location: ../templates/private/paciente/homePaciente.php");
+                exit();
+            }
+        }
+
+        // Verifica se é um funcionário
+        $consulta_funcionario = "SELECT * FROM funcionario WHERE email = ?";
+        $stmt_funcionario = $pdo->prepare($consulta_funcionario);
+        $stmt_funcionario->execute([$email]);
+
+        if ($stmt_funcionario->rowCount() > 0) {
+            $dados_funcionario = $stmt_funcionario->fetch(PDO::FETCH_ASSOC);
+
+            // Verifica a correspondência da senha hash
+            if (password_verify($senha, $dados_funcionario['senha'])) {
+                // Funcionário autenticado com sucesso
+                // Redirecionar para a homepage do funcionário
+                header("location: ../templates/private/funcionario/homeFuncionario.php");
+                exit();
+            }
+        }
+
+        // Se chegou aqui, as credenciais não foram encontradas
+        throw new Exception('Credenciais inválidas');
+
+    } catch (Exception $e) {
+        // Rollback em caso de falha
+        $pdo->rollback();
+        exit('Falha no login: ' . $e->getMessage());
+    }
+}
+?>
