ba373ee694af011171219a58b77cc9176693b91e
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2023-11-12 02:00:17 -0300

    fix: corrigido tabela funcionario

--- tabelas.sql
+++ tabelas.sql
@@ -22,7 +22,7 @@ CREATE TABLE funcionario (
     `data_nascimento` DATE NOT NULL,
     `sexo` VARCHAR(9) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
-    `senha` VARCHAR(255) NOT NULL,
+    `hash_senha` VARCHAR(255) NOT NULL,
     `especialidade` VARCHAR(50) NOT NULL,
     `regpro` VARCHAR(20) NOT NULL,
     `telefone` VARCHAR(20) NOT NULL,
