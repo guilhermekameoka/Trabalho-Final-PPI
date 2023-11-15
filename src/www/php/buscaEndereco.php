48e9ec6aae86329ed6c1bf8f8e995cd95fe01f7c
Guilherme R. Kameoka <80076658+guilhermekameoka@users.noreply.github.com>
2023-11-15 17:15:39 -0300

    feat: busca endereço backend

---
+++ src/www/php/buscaEndereco.php
@@ -0,0 +1,49 @@
+<?php
+require "conexao.php";
+
+class Endereco
+{
+    public $bairro;
+    public $cidade;
+    public $estado;
+
+    function __construct($bairro, $cidade, $estado)
+    {
+        $this->bairro = $bairro;
+        $this->cidade = $cidade;
+        $this->estado = $estado;
+    }
+}
+
+$pdo = mysqlConnect();
+
+$cep = $_GET['cep'] ?? '';
+
+try {
+        $sql = <<<SQL
+        SELECT bairro, cidade, estado
+        FROM paciente WHERE cep = ?
+        UNION
+        SELECT bairro, cidade, estado
+        FROM funcionario WHERE cep = ?
+        SQL;
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$cep, $cep]);
+
+} 
+catch (Exception $e) {
+        exit('Ocorreu uma falha: ' . $e->getMessage());
+}
+
+
+if($row = $stmt->fetch()){
+    $endereco = new Endereco($row['bairro'], $row['cidade'], $row['estado']);
+}
+else{
+    $endereco = new Endereco('', '', '');
+}
+
+echo json_encode($endereco);
+
+?>
